005601851697bd63524bdce714aa0f1b76ae86ca
Arunalu Bamunusinghe <130811052+ArunaluB@users.noreply.github.com>
2024-01-20 23:17:46 +0530

    sales report add  step

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -13,7 +13,7 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/ReportFrom.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/EmployeeDashboadFrom.fxml"))));
         primaryStage.setTitle("HomePage");
         primaryStage.show();
     }
